f54211862d62a4dd0bf82ffa4696a7a2195a3915
Rakesh <ndccroyals@gmail.com>
2019-04-17 23:12:12 -0500

    seed job

---
+++ seed.groovy
@@ -0,0 +1,30 @@
+job('Seed-Job') {
+    description('Seed-Job')
+    environmentVariables {
+        keepBuildVariables(true)
+        keepSystemVariables(true)
+    }
+    scm {
+        git {
+            remote {
+                url("https://github.com/ndccroyals/devops-pipeline.git")
+                credentials("ndccroyals:Royals211016")
+            }
+            branch('master')
+        }
+    }
+    triggers {
+        scm('H/30 * * * *')
+        cron('H/60 H/24 * * *')
+    }
+    concurrentBuild(false)
+
+    steps {
+        dsl {
+            external "*.groovy"
+            removeAction('DELETE')
+            removeViewAction('DELETE')
+            ignoreExisting(false)
+         }
+    }
+}
