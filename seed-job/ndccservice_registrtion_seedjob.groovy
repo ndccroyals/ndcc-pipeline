2d3e04d1b867d91a31dbb7d00e569e6c27c79583
jahnavic <jahnavi.honey54@gmail.com>
2019-05-09 22:10:39 -0500

    added

---
+++ seed-job/ndccservice_registrtion_seedjob.groovy
@@ -0,0 +1,30 @@
+job('ndccservice_registration_seed_job') {
+    description('Seed-Job')
+    environmentVariables {
+        keepBuildVariables(true)
+        keepSystemVariables(true)
+    }
+    scm {
+        git {
+            remote {
+                url("https://github.com/ndccroyals/ndcc-service-registration.git")
+                credentials("ndccroyals:Royals211016")
+            }
+            branch('master')
+        }
+    }
+    triggers {
+        scm('H/30 * * * *')
+        cron('H/60 H/24 * * *')
+    }
+    concurrentBuild(false)
+
+    steps {
+        dsl {
+            external "ndccregistration-service.groovy"
+            removeAction('DELETE')
+            removeViewAction('DELETE')
+            ignoreExisting(false)
+         }
+    }
+}
