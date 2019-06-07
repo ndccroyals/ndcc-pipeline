dccc6ebbaa7ffabee28761481fb799eb0722def6
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-06-06 22:02:01 -0500

    Update ndccservice_registrtion_seedjob.groovy

--- seed-job/ndccservice_registrtion_seedjob.groovy
+++ seed-job/ndccservice_registrtion_seedjob.groovy
@@ -20,7 +20,7 @@ job('ndccservice_registration_seed_job') {
     concurrentBuild(false)
 
     steps {
-        pipeline {
+        pipelineJob {
             external "ndccregistration_service.groovy"
             removeAction('DELETE')
             removeViewAction('DELETE')
